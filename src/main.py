ff049115e60f763839a05321fae6b92caf2fe974
shio <85730998+dino3616@users.noreply.github.com>
2025-01-12 17:56:05 +0000

    feat: ✨ main を実装

--- src/main.py
+++ src/main.py
@@ -1,6 +1,73 @@
-def main() -> None:
-    print("Hello, World!")
+import os
+
+from dotenv import load_dotenv
+
+from infra.gpio_driver import GpioDriver
+from infra.qr_code_reader_driver import QrCodeReaderDriver
+from service import APIClient
+
+load_dotenv()
+
+client = APIClient(base_url=str(os.environ.get("API_BASE_URL")))
+
+gpio_driver = GpioDriver(
+    drawer_id_pin_map={
+        1: 16,
+        2: 28,
+    }
+)
+
+
+def main(user_id: str) -> None:
+    user = client.find_user(user_id)
+    if user is None:
+        client.update_locker_status(
+            locker_id=str(os.environ.get("LOCKER_ID")),
+            type="ERROR",
+            name="認証エラー",
+            description="認証に失敗しました。QRコードが正しいかご確認ください。",
+        )
+        return
+    print(f"Found user. user_id: {user.id}")
+    client.update_locker_status(
+        locker_id=str(os.environ.get("LOCKER_ID")),
+        type="SUCCESS",
+        name="認証成功",
+        description=f"こんにちは、{user.name}さん。",
+    )
+
+    if user.lost_and_found_state == "DELIVERING":
+        drawer = client.put_in_lost_item(user.id)
+        print(f"Put in lost item. drawer_id: {drawer.id}")
+        client.update_locker_status(
+            locker_id=str(os.environ.get("LOCKER_ID")),
+            type="INFO",
+            name="拾得物受付",
+            description="自動でロッカーが開きます。拾得物を収納し、手動でロッカーを閉めてください。",
+        )
+        gpio_driver.open_drawer(drawer.id)
+    elif user.lost_and_found_state == "RETRIEVING":
+        drawer = client.take_out_lost_item(user.id)
+        print(f"Take out lost item. drawer_id: {drawer.id}")
+        client.update_locker_status(
+            locker_id=str(os.environ.get("LOCKER_ID")),
+            type="INFO",
+            name="遺失物受付",
+            description="自動でロッカーが開きます。遺失物を取り出し、手動でロッカーを閉めてください。",
+        )
+        gpio_driver.open_drawer(drawer.id)
+    else:
+        print("Found user, but lost_and_found_state is NONE.")
+        client.update_locker_status(
+            locker_id=str(os.environ.get("LOCKER_ID")),
+            type="WARN",
+            name="ステータスエラー",
+            description="ユーザーのステータスが不正です。管理者にお問い合わせください。",
+        )
 
 
 if __name__ == "__main__":
-    main()
+    gpio_driver.setup()
+
+    qr_code_reader_driver = QrCodeReaderDriver(main, gpio_driver.cleanup)
+    qr_code_reader_driver.start()
